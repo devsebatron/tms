c0b7a5337b155c1a30b5f08ae985caeef5e8bd2d
devsebatron <73515367+devsebatron@users.noreply.github.com>
2023-04-05 20:36:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php 
+<?php
 session_start();
+
+// If the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  // If the user clicked the sign-in button
+  if (isset($_POST['sign-in'])) {
+    // Get the form data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Validate the form data
+    if (empty($email) || empty($password)) {
+      $error = "Please enter your email and password.";
+    } else {
+      // Check if the user exists in the database
+      $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+      $stmt->execute([$email]);
+      $user = $stmt->fetch();
+
+      if ($user && password_verify($password, $user['password'])) {
+        // Authentication successful, create session variable and redirect to home page
+        $_SESSION['user_id'] = $user['id'];
+        header('Location: home.php');
+        exit;
+      } else {
+        // Authentication failed, display error message
+        $error = "Invalid email or password.";
+      }
+    }
+
+  // If the user clicked the sign-up button
+  } elseif (isset($_POST['sign-up'])) {
+    // Get the form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Validate the form data
+    if (empty($name) || empty($email) || empty($password)) {
+      $error = "Please enter your name, email, and password.";
+    } else {
+      // Check if the user already exists in the database
+      $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+      $stmt->execute([$email]);
+      $user = $stmt->fetch();
+
+      if ($user) {
+        // User already exists, display error message
+        $error = "A user with that email already exists.";
+      } else {
+        // Insert the new user into the database
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+        $stmt->execute([$name, $email, $hashed_password]);
+
+        // Redirect to the login page
+        header('Location: login.php');
+        exit;
+      }
+    }
+  }
+}
+
+?>
+
+<?php 
+//session_start();
 include('./db_connect.php');
   ob_start();
   // if(!isset($_SESSION['system'])){
@@ -10,7 +77,7 @@ include('./db_connect.php');
     foreach($system as $k => $v){
       $_SESSION['system'][$k] = $v;
     }
-  // }
+   //}
   ob_end_flush();
 ?>
 <?php 
@@ -19,8 +86,167 @@ header("location:index.php?page=home");
 
 ?>
 <?php include 'header.php' ?>
+<<<<<<< Updated upstream
 <body class="hold-transition login-page bg-black">
 <div class="login-box">
+=======
+
+
+<style>
+  body{
+  margin:0;
+  color:#6a6f8c;
+  background:#c8c8c8;
+  font:600 16px/18px 'Open Sans',sans-serif;
+}
+*,:after,:before{box-sizing:border-box}
+.clearfix:after,.clearfix:before{content:'';display:table}
+.clearfix:after{clear:both;display:block}
+a{color:inherit;text-decoration:none}
+
+.login-wrap{
+  width:100%;
+  margin:auto;
+  max-width:525px;
+  min-height:670px;
+  position:relative;
+  background:url(https://raw.githubusercontent.com/khadkamhn/day-01-login-form/master/img/bg.jpg) no-repeat center;
+  box-shadow:0 12px 15px 0 rgba(0,0,0,.24),0 17px 50px 0 rgba(0,0,0,.19);
+}
+.login-html{
+  width:100%;
+  height:100%;
+  position:absolute;
+  padding:90px 70px 50px 70px;
+  background:rgba(40,57,101,.9);
+}
+.login-html .sign-in-htm,
+.login-html .sign-up-htm{
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  position:absolute;
+  transform:rotateY(180deg);
+  backface-visibility:hidden;
+  transition:all .4s linear;
+}
+.login-html .sign-in,
+.login-html .sign-up,
+.login-form .group .check{
+  display:none;
+}
+.login-html .tab,
+.login-form .group .label,
+.login-form .group .button{
+  text-transform:uppercase;
+}
+.login-html .tab{
+  font-size:22px;
+  margin-right:15px;
+  padding-bottom:5px;
+  margin:0 15px 10px 0;
+  display:inline-block;
+  border-bottom:2px solid transparent;
+}
+.login-html .sign-in:checked + .tab,
+.login-html .sign-up:checked + .tab{
+  color:#fff;
+  border-color:#1161ee;
+}
+.login-form{
+  min-height:345px;
+  position:relative;
+  perspective:1000px;
+  transform-style:preserve-3d;
+}
+.login-form .group{
+  margin-bottom:15px;
+}
+.login-form .group .label,
+.login-form .group .input,
+.login-form .group .button{
+  width:100%;
+  color:#fff;
+  display:block;
+}
+.login-form .group .input,
+.login-form .group .button{
+  border:none;
+  padding:15px 20px;
+  border-radius:25px;
+  background:rgba(255,255,255,.1);
+}
+.login-form .group input[data-type="password"]{
+  text-security:circle;
+  -webkit-text-security:circle;
+}
+.login-form .group .label{
+  color:#aaa;
+  font-size:12px;
+}
+.login-form .group .button{
+  background:#1161ee;
+}
+.login-form .group label .icon{
+  width:15px;
+  height:15px;
+  border-radius:2px;
+  position:relative;
+  display:inline-block;
+  background:rgba(255,255,255,.1);
+}
+.login-form .group label .icon:before,
+.login-form .group label .icon:after{
+  content:'';
+  width:10px;
+  height:2px;
+  background:#fff;
+  position:absolute;
+  transition:all .2s ease-in-out 0s;
+}
+.login-form .group label .icon:before{
+  left:3px;
+  width:5px;
+  bottom:6px;
+  transform:scale(0) rotate(0);
+}
+.login-form .group label .icon:after{
+  top:6px;
+  right:0;
+  transform:scale(0) rotate(0);
+}
+.login-form .group .check:checked + label{
+  color:#fff;
+}
+.login-form .group .check:checked + label .icon{
+  background:#1161ee;
+}
+.login-form .group .check:checked + label .icon:before{
+  transform:scale(1) rotate(45deg);
+}
+.login-form .group .check:checked + label .icon:after{
+  transform:scale(1) rotate(-45deg);
+}
+.login-html .sign-in:checked + .tab + .sign-up + .tab + .login-form .sign-in-htm{
+  transform:rotate(0);
+}
+.login-html .sign-up:checked + .tab + .login-form .sign-up-htm{
+  transform:rotate(0);
+}
+
+.hr{
+  height:2px;
+  margin:60px 0 50px 0;
+  background:rgba(255,255,255,.2);
+}
+.foot-lnk{
+  text-align:center;
+}
+</style>
+<body>
+<!--<div class="login-box">
+>>>>>>> Stashed changes
   <div class="login-logo">
     <a href="#" class="text-white"><b><?php echo $_SESSION['system']['name'] ?> - Admin</b></a>
   </div>
@@ -64,7 +290,65 @@ header("location:index.php?page=home");
     <!-- /.login-card-body -->
   </div>
 </div>
+<<<<<<< Updated upstream
 <!-- /.login-box -->
+=======
+-->
+<div class="login-wrap">
+  <div class="login-html">
+    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+    <div class="login-form">
+      <div class="sign-in-htm">
+        <div class="group">
+          <label for="user" class="label">Username</label>
+          <input id="user" type="text" class="input">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Password</label>
+          <input id="pass" type="password" class="input" data-type="password">
+        </div>
+        <div class="group">
+          <input id="check" type="checkbox" class="check" checked>
+          <label for="check"><span class="icon"></span> Keep me Signed in</label>
+        </div>
+        <div class="group">
+          <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+        </div>
+        <div class="hr"></div>
+        <div class="foot-lnk">
+          <a href="#forgot">Forgot Password?</a>
+        </div>
+      </div>
+      <div class="sign-up-htm">
+        <div class="group">
+          <label for="user" class="label">Username</label>
+          <input id="user" type="text" class="input">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Password</label>
+          <input id="pass" type="password" class="input" data-type="password">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Repeat Password</label>
+          <input id="pass" type="password" class="input" data-type="password">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Email Address</label>
+          <input id="pass" type="text" class="input">
+        </div>
+        <div class="group">
+          <input type="submit" class="button" value="Sign Up">
+        </div>
+        <div class="hr"></div>
+        <div class="foot-lnk">
+          <label for="tab-1">Already Member?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+>>>>>>> Stashed changes
 <script>
   $(document).ready(function(){
     $('#login-form').submit(function(e){
