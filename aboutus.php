51a7cba91c9cc7b719b5bf50a50b3460f73fa0bf
devsebatron <73515367+devsebatron@users.noreply.github.com>
2023-04-06 22:41:00 +0530

    pic

--- aboutus.php
+++ aboutus.php
@@ -1,47 +1,48 @@
-<?php include'db_connect.php' ?>
-<div class="col-lg-12">
-	<div class="card card-outline card-success">
-		<div class="card-header">
-			<div class="card-tools">
-				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New project</a>
+<?php include 'db_connect.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us - KIN Task Management System</title>
+	<!-- include your CSS and JavaScript files here -->
+</head>
+<body>
+	<div class="container">
+		<h1>About Us</h1>
+		<p>KIN Task Management System is a project management tool developed by a team of dedicated developers.</p>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="developer1.jpg" alt="Developer 1" class="img-fluid rounded-circle mb-2" width="150">
+						<h3>Innama Ashgar</h3>
+						<p class="text-secondary">Lead Developer</p>
+						<p>John has over 10 years of experience in software development. He specializes in front-end development and user interface design.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="assets\uploads\nevin.jpg" alt="Developer 2" class="img-fluid rounded-circle mb-2" width="150">
+						<h3>Nevin Sebastian</h3>
+						<p class="text-secondary">Back-end Developer</p>
+						<p>Jane is an expert in server-side programming languages such as PHP and Node.js. She enjoys optimizing code and improving website performance.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="developer3.jpg" alt="Developer 3" class="img-fluid rounded-circle mb-2" width="150">
+						<h3>Kubber Bansal</h3>
+						<p class="text-secondary">Full-stack Developer</p>
+						<p>Mike is a jack-of-all-trades developer who enjoys building robust and scalable web applications. He's passionate about open-source technologies and is always learning new skills.</p>
+					</div>
+				</div>
 			</div>
 		</div>
-	<div class="card-body">
-</div>
-    </div>
-</div>
-<style>
-	table p{
-		margin: unset !important;
-	}
-	table td{
-		vertical-align: middle !important
-	}
-</style>
-<script>
-	$(document).ready(function()
-	{
-		$('#list').dataTable()
-		$('.new_productivity').click(function()
-		{
-		uni_modal("<i class='fa fa-plus'></i> New Progress for: "+$(this).attr('data-task'),"manage_progress.php?pid="+$(this).attr('data-pid')+"&tid="+$(this).attr('data-tid'),'large')
-	})
-	})
-	function delete_project($id){
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=delete_project',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
-
-				}
-			}
-		})
-	}
-</script>
+		<p>At KIN Task Management System, we're committed to providing a user-friendly and efficient project management tool for businesses of all sizes. Whether you're a small startup or a large enterprise, our software can help streamline your workflow and increase productivity.</p>
+		<p>Contact us today to learn more about our product and how we can help your business succeed.</p>
+	</div>
+</body>
+</html>
