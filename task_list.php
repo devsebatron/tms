ad96ea61add47da8643e5d5ca3c5f7cd686734f3
devsebatron <73515367+devsebatron@users.noreply.github.com>
2023-04-15 11:31:06 +0530

    Update task_list.php

--- task_list.php
+++ task_list.php
@@ -3,7 +3,7 @@
 	<div class="card card-outline card-success">
 		<div class="card-header">
 			<div class="card-tools">
-				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New project</a>
+				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New Task</a>
 			</div>
 		</div>
 		<div class="card-body">
